e5b453b43fe2659347f780e5fc823ad0fd6f5621
vega jafri <giantwhitedeer@gmail.com>
2025-11-06 18:42:31 -0500

    asdfghjk

---
+++ adaptation-game/Assets/Scenes/minigame2/scripts/DragObjects.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Video;
+
+public class DragObjects : MonoBehaviour
+{
+    Vector3 startPos;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 move = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        move.z = 0;
+
+        transform.position = move;
+    }
+
+    private void OnMouseUp()
+    {
+        transform.position = startPos;
+    }
+}
