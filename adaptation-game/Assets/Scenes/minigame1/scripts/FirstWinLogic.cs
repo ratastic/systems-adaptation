a30fef7a8106790e0ca9b6ccd806d88726bbdfb7
vega jafri <giantwhitedeer@gmail.com>
2025-11-10 23:04:41 -0500

    fixes

--- adaptation-game/Assets/Scenes/minigame1/scripts/FirstWinLogic.cs
+++ adaptation-game/Assets/Scenes/minigame1/scripts/FirstWinLogic.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class FirstWinLogic : MonoBehaviour
 {
+    public Collider2D leftStrap;
+    public Collider2D leftSnap;
+    public Collider2D rightStrap;
+    public Collider2D rightSnap;
+
+    public SpriteRenderer mirrorSprite;
+    public Sprite happy;
+
+    public GameObject grid1;
+    public GameObject grid2;
+    public GameObject nextButton;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        grid1.SetActive(true);
+        grid2.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        BraClasped();
+    }
+
+    public void BraClasped()
+    {
+        if (leftStrap.IsTouching(leftSnap) && rightStrap.IsTouching(rightSnap))
+        {
+            mirrorSprite.sprite = happy;
+            grid1.SetActive(false);
+            grid2.SetActive(true);
+            nextButton.SetActive(true);
+            Debug.Log("thank you");
+        }
     }
 }
