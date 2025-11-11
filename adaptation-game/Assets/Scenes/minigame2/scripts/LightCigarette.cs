3c54b7656d307e490f5fb8028e8456ea3924cd58
vega jafri <giantwhitedeer@gmail.com>
2025-11-11 04:52:43 -0500

    edits

--- adaptation-game/Assets/Scenes/minigame2/scripts/LightCigarette.cs
+++ adaptation-game/Assets/Scenes/minigame2/scripts/LightCigarette.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class LightCigarette : MonoBehaviour
 {
+    public Collider2D cig;
+    private BoxCollider2D lighter;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        lighter.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Lighting();
     }
+
+    public void Lighting()
+    {
+        if (lighter.IsTouching(cig))
+        {
+            Debug.Log("lit cig");
+        }
+    }
+
 }
