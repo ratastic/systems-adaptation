e1ec27dc1c83ab001d7642547e8642419dde2fa8
vega jafri <giantwhitedeer@gmail.com>
2025-11-12 16:04:42 -0500

    changes

---
+++ adaptation-game/Assets/Scenes/minigame3/scripts/MoveCam.cs
@@ -0,0 +1,119 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using System;
+
+public class MoveCam : MonoBehaviour
+{
+    public GameObject arrows;
+    private Transform camScreen;
+    Vector3 startPos;
+    Vector3 focusPos = new Vector3(6, 0, 0);
+    Vector3 previousMousePos;
+    Vector3 currentMousePos;
+    private bool inFrame;
+    private int moveAmount = 1;
+    private float mouseSensitivity = 0.5f;
+
+    public SpriteRenderer idleSprite;
+    public Sprite poseSprite;
+    private Sprite startSprite;
+
+    public GameObject questionText;
+
+    [Header("cam bounds")]
+    public float minX = -5f;
+    public float maxX = 10f;
+    public float minY = -3f;
+    public float maxY = 3f;
+        
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        camScreen = GetComponent<Transform>();
+        startPos = camScreen.position;
+        inFrame = false;
+        
+        Vector3 mouseStart = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        previousMousePos = mouseStart;
+
+        startSprite = idleSprite.sprite;
+        questionText.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Destroy(arrows);
+        }
+
+        OnMouseDrag();
+
+        if (camScreen.position == focusPos)
+        {
+            inFrame = true;
+            idleSprite.sprite = poseSprite;
+            questionText.SetActive(false);
+
+            if (inFrame == true)
+            {
+                TakePicture();
+            }
+        } else
+        {
+            idleSprite.sprite = startSprite;
+            questionText.SetActive(true);
+        }
+    }
+
+    private void OnMouseDrag()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            currentMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 delta = currentMousePos - previousMousePos;
+
+            if (delta.x > mouseSensitivity)
+            {
+                transform.position = new Vector3(camScreen.position.x + moveAmount, camScreen.position.y, camScreen.position.z);
+                previousMousePos = currentMousePos;
+            }
+            else if (delta.x < -mouseSensitivity)
+            {
+                transform.position = new Vector3(camScreen.position.x - moveAmount, camScreen.position.y, camScreen.position.z);
+                previousMousePos = currentMousePos;
+            }
+
+            if (delta.y > mouseSensitivity)
+            {
+                transform.position = new Vector3(camScreen.position.x, camScreen.position.y + moveAmount, camScreen.position.z);
+                previousMousePos = currentMousePos;
+            }
+            else if (delta.y < -mouseSensitivity)
+            {
+                transform.position = new Vector3(camScreen.position.x, camScreen.position.y - moveAmount, camScreen.position.z);
+                previousMousePos = currentMousePos;
+            }
+        }
+
+        transform.position = new Vector3
+        (
+            Mathf.Clamp(transform.position.x, minX, maxX),
+            Mathf.Clamp(transform.position.y, minY, maxY),
+            transform.position.z
+        );
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            previousMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+    private void TakePicture()
+    {
+        Debug.Log("can take picture");
+    }
+}
